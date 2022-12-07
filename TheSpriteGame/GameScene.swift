eda8d95ce11a43ce13b98d8b6a364f838f129d8a
Michal <s22845@pjwstk.edu.pl>
2022-12-07 19:26:10 +0100

    AirTag game completed

--- TheSpriteGame/GameScene.swift
+++ TheSpriteGame/GameScene.swift
@@ -8,81 +8,80 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    private var airtags: [SKSpriteNode] = []
+    
     override func didMove(to view: SKView) {
+//        let cameraNode = SKCameraNode()
+//        cameraNode.position = CGPoint(x: cameraNode.frame.size.width / 2, y: cameraNode.frame.size.height / 2)
+//
+//        addChild(cameraNode)
+//        camera = cameraNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let bg = SKSpriteNode(imageNamed: "wooden_floor")
+        bg.blendMode = .replace
+        bg.zPosition = -1
+        bg.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        self.addChild(bg)
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let destroyer = SKShapeNode(rectOf: CGSize(width: 200, height: 10))
+        destroyer.fillColor = .red
+        destroyer.physicsBody = SKPhysicsBody(rectangleOf: destroyer.frame.size)
+        destroyer.physicsBody?.isDynamic = false
+        destroyer.position = CGPoint(x: 200, y: 100)
+        destroyer.name = "destroyer"
+        addChild(destroyer)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    func didBegin(_ contact: SKPhysicsContact){
+        guard let bodyA = contact.bodyA.node else {return}
+        guard let bodyB = contact.bodyB.node else {return}
+        
+        print("collision \(bodyA.name) \(bodyB.name)")
+        
+        if bodyA.name == "airtag" && bodyB.name == "destroyer"{
+            bodyA.removeFromParent()
+        } else if bodyB.name == "airtag" && bodyA.name == "destroyer"{
+            bodyB.removeFromParent()
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let airtag = SKSpriteNode(imageNamed: "airtag")
+            airtag.name = "airtag"
+            airtag.position = CGPoint(x: touch.location(in: self).x, y: touch.location(in: self).y)
+            let clickedNotes = self.nodes(at: position)
+            
+            airtag.blendMode = .alpha
+            airtag.physicsBody = SKPhysicsBody(circleOfRadius: airtag.size.width / 2)
+            airtag.physicsBody?.affectedByGravity = false
+            airtag.physicsBody?.contactTestBitMask = physicsBody!.collisionBitMask
+            airtags.append(airtag)
+            self.addChild(airtag)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            if !airtags.isEmpty {
+                let activeAirtag = airtags.last
+                activeAirtag?.position = CGPoint(x: touch.location(in: self).x, y: touch.location(in: self).y)
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if !airtags.isEmpty {
+            let activeAirtag = airtags.last
+            activeAirtag!.physicsBody?.affectedByGravity = true
+            activeAirtag!.physicsBody?.restitution = 0.9
+        }
     }
 }
